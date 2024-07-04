d83b504a379f400d356495d7b4c246dfe4a20afe
Artem Kulakov <109291017+temakulakov@users.noreply.github.com>
2024-07-04 17:36:45 +0300

    Прошедшие события и месяца не показываются

--- src/App.tsx
+++ src/App.tsx
@@ -1,113 +1,221 @@
-import React, { useRef } from 'react';
-import Header from "./components/Header/Header";
-import { useQuery } from "@tanstack/react-query";
-import { getList } from "./services/bitrix";
-import styles from './App.module.scss';
-import Footer from "./components/Footer/Footer";
-import Slider from "./components/Slider/Slider";
-import Grid from "./components/Grid/Grid";
-import Line from "./components/Line/Line";
+import { useQuery } from '@tanstack/react-query';
 import dayjs from 'dayjs';
-import { IEvent } from "./types";
+import React, { useRef } from 'react';
 import { isMobile } from 'react-device-detect';
-import defaultt from './static/images/default.jpg'
+import styles from './App.module.scss';
+import Footer from './components/Footer/Footer';
+import Grid from './components/Grid/Grid';
+import Header from './components/Header/Header';
+import Line from './components/Line/Line';
+import Slider from './components/Slider/Slider';
+import { getList } from './services/bitrix';
+import defaultt from './static/images/default.jpg';
+import { IEvent } from './types';
 
-import logo from './static/images/BTM.svg'
-import logo2 from './static/logo/logo.png'
+import logo from './static/images/BTM.svg';
+import logo2 from './static/logo/logo.png';
 
 const months = ['Июнь', 'Июль', 'Август'];
 
 interface Month {
-    id: number;
-    title: string;
-    color: string;
-    count: number;
+	id: number;
+	title: string;
+	color: string;
+	count: number;
 }
 
 const monthsJSX: Month[] = [
-    { id: 0, title: 'Июнь', color: '#F8B501', count: 6 },
-    { id: 1, title: 'Июль', color: '#A31A30', count: 7 },
-    { id: 2, title: 'Август', color: '#038388', count: 8 },
+	{ id: 0, title: 'Июнь', color: '#F8B501', count: 6 },
+	{ id: 1, title: 'Июль', color: '#A31A30', count: 7 },
+	{ id: 2, title: 'Август', color: '#038388', count: 8 },
 ];
 
 const App: React.FC = () => {
-    const { data, isLoading, isError } = useQuery({
-        queryKey: ['list'],
-        queryFn: getList
-    });
+	const { data, isLoading, isError } = useQuery({
+		queryKey: ['list'],
+		queryFn: getList,
+	});
 
-    const sectionRefs = useRef<(HTMLDivElement | null)[]>([]);
-    const headingRefs = useRef<(HTMLHeadingElement | null)[]>([]);
+	const sectionRefs = useRef<(HTMLDivElement | null)[]>([]);
+	const headingRefs = useRef<(HTMLHeadingElement | null)[]>([]);
 
-    const scrollToSection = (index: number) => {
-        if (isMobile && headingRefs.current[index]) {
-            headingRefs.current[index]?.scrollIntoView({ behavior: 'smooth' });
-        } else {
-            sectionRefs.current[index]?.scrollIntoView({ behavior: 'smooth' });
-        }
-    };
+	const scrollToSection = (index: number) => {
+		if (isMobile && headingRefs.current[index]) {
+			headingRefs.current[index]?.scrollIntoView({ behavior: 'smooth' });
+		} else {
+			sectionRefs.current[index]?.scrollIntoView({ behavior: 'smooth' });
+		}
+	};
 
-    const getSortedEvents = (events: IEvent[], month: number) => {
-        return events
-            .filter(event => dayjs(event.dateTime).month() === month)
-            .sort((a, b) => dayjs(a.dateTime).isBefore(b.dateTime) ? -1 : 1);
-    };
+	const getSortedEvents = (events: IEvent[], month: number) => {
+		return events
+			.filter(event => dayjs(event.dateTime).month() === month)
+			.sort((a, b) => (dayjs(a.dateTime).isBefore(b.dateTime) ? -1 : 1));
+	};
 
-    console.log(data);
+	const getFutureEvents = (events: IEvent[]) => {
+		const today = dayjs();
+		return events.filter(event => dayjs(event.dateTime).isAfter(today));
+	};
 
-    return (
-        <div className={styles.root}>
-            {
-                isMobile ? <div className={styles.mobile}>
-                    <div className={styles.mobileRoot}>
-                        <div className={styles.mobileTop}>
-                            <a href={'https://gctm.ru'}><img src={logo} alt="Логотип" /></a>
-                            <p className={styles.text}>
-                                Проведи свое лето с пользой!<br />
-                                <span style={{ color: '#A41930' }}>Арт-Лето в Бахрушинском музее</span> —
-                                программа <br /> мастер-классов для детей
-                            </p>
-                        </div>
-                        <a href={'https://gctm.ru'}><img src={logo2} alt="Логотип 2" /></a>
-                    </div>
-                    <div className={styles.mobilePanel}>
-                        {
-                            months.map((el, index) => <button key={index} onClick={() => scrollToSection(index)}>
-                                {el}
-                            </button>)
-                        }
-                    </div>
-                </div> : <>
-                    <Header scrollToSection={scrollToSection} />
-                    <Slider />
-                </>
-            }
-            {isLoading && <>
-                {!isMobile ? <Line color={monthsJSX[0].color} word={monthsJSX[0].title} /> : <h3 style={{marginLeft: '1.5vw'}}>{monthsJSX[0].title}</h3>}
-                <Grid events={[
-                    { id: 0, title: 'Мастер-класс «Узоры Гончаровой и лабиринты»', duration: 60, eventLink: '', ticketLink: '', rating: 6, type: 'Мастер класс', description: 'Ребята познакомятся с эскизами декораций таких знаменитых театральных художников, как Н.Гончарова и И.Билибин. После изучения эскизов декораций к известнейшим спектаклям детям предложат придумать и изобразить свои собственные сценические декорации на воздушной пене яркими красками. Затем все участники перенесут узоры на бумагу и заберут на память яркие рисунки... которым, возможно, предстоит прославиться!', imageLink: defaultt, dateTime: dayjs() },
-                    { id: 1, title: 'Мастер-класс «Узоры Гончаровой и лабиринты»', duration: 60, eventLink: '', ticketLink: '', rating: 6, type: 'Мастер класс', description: 'Ребята познакомятся с эскизами декораций таких знаменитых театральных художников, как Н.Гончарова и И.Билибин. После изучения эскизов декораций к известнейшим спектаклям детям предложат придумать и изобразить свои собственные сценические декорации на воздушной пене яркими красками. Затем все участники перенесут узоры на бумагу и заберут на память яркие рисунки... которым, возможно, предстоит прославиться!', imageLink: defaultt, dateTime: dayjs() },
-                    { id: 2, title: 'Мастер-класс «Узоры Гончаровой и лабиринты»', duration: 60, eventLink: '', ticketLink: '', rating: 6, type: 'Мастер класс', description: 'Ребята познакомятся с эскизами декораций таких знаменитых театральных художников, как Н.Гончарова и И.Билибин. После изучения эскизов декораций к известнейшим спектаклям детям предложат придумать и изобразить свои собственные сценические декорации на воздушной пене яркими красками. Затем все участники перенесут узоры на бумагу и заберут на память яркие рисунки... которым, возможно, предстоит прославиться!', imageLink: defaultt, dateTime: dayjs() },
-                    { id: 3, title: 'Мастер-класс «Узоры Гончаровой и лабиринты»', duration: 60, eventLink: '', ticketLink: '', rating: 6, type: 'Мастер класс', description: 'Ребята познакомятся с эскизами декораций таких знаменитых театральных художников, как Н.Гончарова и И.Билибин. После изучения эскизов декораций к известнейшим спектаклям детям предложат придумать и изобразить свои собственные сценические декорации на воздушной пене яркими красками. Затем все участники перенесут узоры на бумагу и заберут на память яркие рисунки... которым, возможно, предстоит прославиться!', imageLink: defaultt, dateTime: dayjs() },
-                ]} />
-            </>}
-            {!isLoading && !isError && monthsJSX.map((month, index) => (
-                <div
-                    key={month.id}
-                    ref={el => sectionRefs.current[index] = el}
-                    style={{ height: "fit-content", minHeight: "100px" }}
-                >
-                    {!isMobile ? <Line color={month.color} word={month.title} /> : <h3  style={{marginLeft: '1.5vw'}} ref={el => headingRefs.current[index] = el}>{month.title}</h3>}
-                    {data ? (
-                        <Grid events={getSortedEvents(data, month.count - 1)} />
-                    ) : (
-                        <Line color={monthsJSX[0].color} word={monthsJSX[0].title} />
-                    )}
-                </div>
-            ))}
-            <Footer scrollToSection={scrollToSection}/>
-        </div>
-    );
+	const renderMonthSection = (
+		month: Month,
+		events: IEvent[],
+		index: number
+	) => {
+		const futureEvents = getFutureEvents(events);
+		if (futureEvents.length === 0) return null;
+
+		return (
+			<div
+				key={month.id}
+				ref={el => (sectionRefs.current[index] = el)}
+				style={{ height: 'fit-content', minHeight: '100px' }}
+			>
+				{!isMobile ? (
+					<Line color={month.color} word={month.title} />
+				) : (
+					<h3
+						style={{ marginLeft: '1.5vw' }}
+						ref={el => (headingRefs.current[index] = el)}
+					>
+						{month.title}
+					</h3>
+				)}
+				<Grid events={futureEvents} />
+			</div>
+		);
+	};
+
+	console.log(data);
+
+	return (
+		<div className={styles.root}>
+			{isMobile ? (
+				<div className={styles.mobile}>
+					<div className={styles.mobileRoot}>
+						<div className={styles.mobileTop}>
+							<a href={'https://gctm.ru'}>
+								<img src={logo} alt='Логотип' />
+							</a>
+							<p className={styles.text}>
+								Проведи свое лето с пользой!
+								<br />
+								<span style={{ color: '#A41930' }}>
+									Арт-Лето в Бахрушинском музее
+								</span>{' '}
+								— программа <br /> мастер-классов для детей
+							</p>
+						</div>
+						<a href={'https://gctm.ru'}>
+							<img src={logo2} alt='Логотип 2' />
+						</a>
+					</div>
+					<div className={styles.mobilePanel}>
+						{months.map((el, index) => (
+							<button key={index} onClick={() => scrollToSection(index)}>
+								{el}
+							</button>
+						))}
+					</div>
+				</div>
+			) : (
+				<>
+					<Header scrollToSection={scrollToSection} />
+					<Slider />
+				</>
+			)}
+			{isLoading && (
+				<>
+					{!isMobile ? (
+						<Line color={monthsJSX[1].color} word={monthsJSX[1].title} />
+					) : (
+						<h3 style={{ marginLeft: '1.5vw' }}>{monthsJSX[1].title}</h3>
+					)}
+					<Grid
+						events={[
+							{
+								id: 0,
+								title: 'Мастер-класс «Узоры Гончаровой и лабиринты»',
+								duration: 60,
+								eventLink: '',
+								ticketLink: '',
+								rating: 6,
+								type: 'Мастер класс',
+								description:
+									'Ребята познакомятся с эскизами декораций таких знаменитых театральных художников, как Н.Гончарова и И.Билибин. После изучения эскизов декораций к известнейшим спектаклям детям предложат придумать и изобразить свои собственные сценические декорации на воздушной пене яркими красками. Затем все участники перенесут узоры на бумагу и заберут на память яркие рисунки... которым, возможно, предстоит прославиться!',
+								imageLink: defaultt,
+								dateTime: dayjs(),
+							},
+							{
+								id: 1,
+								title: 'Мастер-класс «Узоры Гончаровой и лабиринты»',
+								duration: 60,
+								eventLink: '',
+								ticketLink: '',
+								rating: 6,
+								type: 'Мастер класс',
+								description:
+									'Ребята познакомятся с эскизами декораций таких знаменитых театральных художников, как Н.Гончарова и И.Билибин. После изучения эскизов декораций к известнейшим спектаклям детям предложат придумать и изобразить свои собственные сценические декорации на воздушной пене яркими красками. Затем все участники перенесут узоры на бумагу и заберут на память яркие рисунки... которым, возможно, предстоит прославиться!',
+								imageLink: defaultt,
+								dateTime: dayjs(),
+							},
+							{
+								id: 2,
+								title: 'Мастер-класс «Узоры Гончаровой и лабиринты»',
+								duration: 60,
+								eventLink: '',
+								ticketLink: '',
+								rating: 6,
+								type: 'Мастер класс',
+								description:
+									'Ребята познакомятся с эскизами декораций таких знаменитых театральных художников, как Н.Гончарова и И.Билибин. После изучения эскизов декораций к известнейшим спектаклям детям предложат придумать и изобразить свои собственные сценические декорации на воздушной пене яркими красками. Затем все участники перенесут узоры на бумагу и заберут на память яркие рисунки... которым, возможно, предстоит прославиться!',
+								imageLink: defaultt,
+								dateTime: dayjs(),
+							},
+							{
+								id: 3,
+								title: 'Мастер-класс «Узоры Гончаровой и лабиринты»',
+								duration: 60,
+								eventLink: '',
+								ticketLink: '',
+								rating: 6,
+								type: 'Мастер класс',
+								description:
+									'Ребята познакомятся с эскизами декораций таких знаменитых театральных художников, как Н.Гончарова и И.Билибин. После изучения эскизов декораций к известнейшим спектаклям детям предложат придумать и изобразить свои собственные сценические декорации на воздушной пене яркими красками. Затем все участники перенесут узоры на бумагу и заберут на память яркие рисунки... которым, возможно, предстоит прославиться!',
+								imageLink: defaultt,
+								dateTime: dayjs(),
+							},
+						]}
+					/>
+				</>
+			)}
+			{!isLoading &&
+				!isError &&
+				monthsJSX.map((month, index) => (
+					<div key={month.id} ref={el => (sectionRefs.current[index] = el)}>
+						{!isMobile ? (
+							<Line color={month.color} word={month.title} />
+						) : (
+							<h3
+								style={{ marginLeft: '1.5vw' }}
+								ref={el => (headingRefs.current[index] = el)}
+							>
+								{month.title}
+							</h3>
+						)}
+						{data &&
+						getFutureEvents(getSortedEvents(data, month.count - 1)).length >
+							0 ? (
+							<Grid
+								events={getFutureEvents(getSortedEvents(data, month.count - 1))}
+							/>
+						) : null}
+					</div>
+				))}
+			<Footer scrollToSection={scrollToSection} />
+		</div>
+	);
 };
 
 export default App;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,58 +1,72 @@
 import React, { useState } from 'react';
-import styles from './Header.module.scss';
-import logo from '../../static/images/BTM.svg';
+import dz from '../../static/icons/dz.svg';
+import ok from '../../static/icons/ok.svg';
+import phone from '../../static/icons/phone.svg';
+import rt from '../../static/icons/rt.svg';
 import tg from '../../static/icons/tg.svg';
 import vk from '../../static/icons/vk.svg';
-import ok from '../../static/icons/ok.svg';
-import dz from '../../static/icons/dz.svg';
-import rt from '../../static/icons/rt.svg';
-import phone from '../../static/icons/phone.svg';
+import logo from '../../static/images/BTM.svg';
+import styles from './Header.module.scss';
 
 interface HeaderProps {
-    scrollToSection: (index: number) => void;
+	scrollToSection: (index: number) => void;
 }
 
 const Header: React.FC<HeaderProps> = ({ scrollToSection }) => {
-    const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+	const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
 
-    const months = [
-        { title: 'Июнь', color: '#F8B501' },
-        { title: 'Июль', color: '#A31A30' },
-        { title: 'Август', color: '#038388' }
-    ];
+	const months = [
+		{ title: 'Июнь', color: '#F8B501' },
+		{ title: 'Июль', color: '#A31A30' },
+		{ title: 'Август', color: '#038388' },
+	];
 
-    return (
-        <div className={styles.root}>
-            <a href={'https://gctm.ru'}><img className={styles.logo} src={logo} alt="Logo" /></a>
-            <ul className={styles.menu}>
-                {months.map((month, i) => (
-                    <li
-                        key={i}
-                        onClick={() => scrollToSection(i)}
-                        onMouseEnter={() => setHoveredIndex(i)}
-                        onMouseLeave={() => setHoveredIndex(null)}
-                        style={{ backgroundColor: hoveredIndex === i ? month.color : '#F8B501' }}
-                    >
-                        {month.title}
-                    </li>
-                ))}
-            </ul>
-            <div className={styles.menu}>
-                <a href={'https://t.me/bakhrushinmuseum'}><img src={tg} alt="Telegram" /></a>
-                <a href={'https://vk.com/bahrushinmuseum'}><img src={vk} alt="VK" /></a>
-                <a href={'https://ok.ru/bakhrushinmuseum'}><img src={ok} alt="OK" /></a>
-                <a href={'https://dzen.ru/bahrushinmuseum'}><img src={dz} alt="DZ" /></a>
-                <a href={'https://rutube.ru/channel/23745556/'}><img src={rt} alt="RT" /></a>
-            </div>
-            <div className={styles.telephone}>
-                <img src={phone} alt="Phone" />
-                <div className={styles.col}>
-                    <a href="tel:+74994847777">+7 /499/ 484-77-77</a>
-                    <a href="tel:+79636594088">+7 963 659 40 88</a>
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div className={styles.root}>
+			<a href={'https://gctm.ru'}>
+				<img className={styles.logo} src={logo} alt='Logo' />
+			</a>
+			<ul className={styles.menu}>
+				{months.map((month, i) => (
+					<li
+						key={i}
+						onClick={() => scrollToSection(i)}
+						onMouseEnter={() => setHoveredIndex(i)}
+						onMouseLeave={() => setHoveredIndex(null)}
+						style={{
+							backgroundColor: hoveredIndex === i ? month.color : '#F8B501',
+						}}
+					>
+						{month.title}
+					</li>
+				))}
+			</ul>
+			<div className={styles.menu}>
+				<a href={'https://t.me/bakhrushinmuseum'}>
+					<img src={tg} alt='Telegram' />
+				</a>
+				<a href={'https://vk.com/bahrushinmuseum'}>
+					<img src={vk} alt='VK' />
+				</a>
+				<a href={'https://ok.ru/bakhrushinmuseum'}>
+					<img src={ok} alt='OK' />
+				</a>
+				<a href={'https://dzen.ru/bahrushinmuseum'}>
+					<img src={dz} alt='DZ' />
+				</a>
+				<a href={'https://rutube.ru/channel/23745556/'}>
+					<img src={rt} alt='RT' />
+				</a>
+			</div>
+			<div className={styles.telephone}>
+				<img src={phone} alt='Phone' />
+				<div className={styles.col}>
+					<a href='tel:+74994847777'>+7 /499/ 484-77-77</a>
+					<a href='tel:+79636594088'>+7 963 659 40 88</a>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Header;
